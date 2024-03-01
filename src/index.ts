deadb610a2a9355567096f39ebe7932d4e44abe1
Guilherme-07062002 <guilherme762002@gmail.com>
2024-03-01 19:36:40 -0300

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({
+    message: "Hello, World!"
+  })
+});
+
+server.listen(3000, () => {
+  console.log('\nlistening on 3000');
+});
