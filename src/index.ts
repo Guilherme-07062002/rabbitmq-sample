ea4faca95241c1472b395f242160e52ef42a7eeb
Guilherme-07062002 <guilherme762002@gmail.com>
2024-03-01 20:12:29 -0300

    feat add amqp messaging funcionality sample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import client, { Connection, Channel, ConsumeMessage } from 'amqplib';
 import express from 'express';
 import http from 'http';
 
@@ -15,3 +16,26 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
   console.log('\nlistening on 3000');
 });
+
+function sendMessages(channel: Channel): void {
+  for (let i = 0; i < 100; i++) {
+    channel.sendToQueue('queue', Buffer.from('Hello, World!'));
+  }
+}
+
+const consumer = (channel: Channel) => (msg: ConsumeMessage | null): void => {
+  if (msg) {
+    console.log(msg.content.toString());
+    channel.ack(msg);
+  }
+}
+
+const go = async () => {
+  const connection: Connection = await client.connect('amqp://username:password@localhost:5672');
+  const channel: Channel = await connection.createChannel();
+  await channel.assertQueue('queue');
+  sendMessages(channel);
+  channel.consume('queue', consumer(channel));
+}
+
+go();
